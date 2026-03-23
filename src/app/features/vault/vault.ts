2d82ce5adb9f45f93c5f65f07f4c08f0cc472822
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-23 17:47:24 +0400

    feat(vault): vault UI implement

--- src/app/features/vault/vault.ts
+++ src/app/features/vault/vault.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vault',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vault.html',
-  styleUrl: './vault.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Vault {}
