ccf5595db4ce3b02b8dc90d4bb7653b94ec2fd46
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-23 16:00:14 +0400

    feat(structure): setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Auth } from './features/auth/auth';
+import { Dashboard } from './features/dashboard/dashboard';
+import { Ledger } from './features/ledger/ledger';
+import { Markets } from './features/markets/markets';
+import { Vault } from './features/vault/vault';
+import { MainLayout } from './layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Auth },
+
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: 'dashboard', component: Dashboard },
+      { path: 'markets', component: Markets },
+      { path: 'vault', component: Vault },
+      { path: 'ledger', component: Ledger },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    ],
+  },
+
+  { path: '**', redirectTo: 'login' },
+];
