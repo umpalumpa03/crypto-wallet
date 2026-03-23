153c18bab03c769cbe13f00b6e926ad5ee66eb6c
Ucha Sephiskveradze <ucha.sephiskveradze@gmail.com>
2026-03-23 17:49:42 +0400

    feat(ledger): ledger UI implement

--- src/app/features/ledger/ledger.ts
+++ src/app/features/ledger/ledger.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ledger',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './ledger.html',
-  styleUrl: './ledger.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Ledger {}
